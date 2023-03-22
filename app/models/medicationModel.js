75ad4a5ab503da961c1b449383ec0a2362a1e09b
Vihanga <vihangarajalingam7@gmail.com>
2023-03-22 23:54:48 +0530

    Created medication flow
    Created medication model

---
+++ app/models/medicationModel.js
@@ -0,0 +1,32 @@
+import { DataTypes } from 'sequelize';
+import sequelizeCC from './db.js';
+
+// Define medicine model with validations
+const Medications = sequelizeCC.define('medications', {
+    name: {
+        type: DataTypes.STRING, allowNull: false,
+        validate: {
+            is: { args: /^[a-zA-Z0-9_-]+$/i, msg: 'Name can only contain alphanumeric characters, _ and -' }
+        }
+    },
+    weight: {
+        type: DataTypes.DOUBLE, allowNull: false,
+        validate: {
+            isNumeric: { msg: 'Weight should be numeric' },
+            max: { args: 500, msg: 'Max weight is 500' }
+        }
+    },
+    code: {
+        type: DataTypes.STRING, allowNull: false,
+        validate: {
+            is: { args: /^[A-Z0-9_]+$/i, msg: 'Name can only contain uppercase letters, numbers and _' }
+        }
+    },
+    image: {
+        type: DataTypes.BLOB
+    }
+});
+
+await Medications.sync(); // Create table in DB if it doesn't exist
+
+export default Medications;
