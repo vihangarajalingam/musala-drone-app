690bb9a40170701068211950ca64d680c076e192
Vihanga <vihangarajalingam7@gmail.com>
2023-03-24 00:17:43 +0530

    Created flow to get idle drones

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,5 +6,6 @@ var router = express.Router();
 router.post('/drone/create', droneController.create);
 router.post('/drone/load', droneController.load);
 router.get('/drone/get-load', droneController.getLoadedMedication);
+router.get('/drone/get-idle-drones', droneController.getAvailableDrones);
 
 export default router;
