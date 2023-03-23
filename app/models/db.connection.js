abf91ff3d0cb6fc3a6496a56b7850246d856b896
Vihanga <vihangarajalingam7@gmail.com>
2023-03-24 01:20:52 +0530

    Update db.connection.js

--- app/models/db.connection.js
+++ app/models/db.connection.js
@@ -1,5 +1,9 @@
 import Sequelize from "sequelize";
+import mysql from "mysql2";
 import { HOST, USER, PASSWORD, DB, PORT } from "../config/db.config.js";
+
+const connection = mysql.createConnection({ host: HOST, port: PORT, user: USER, password: PASSWORD });
+connection.query(`CREATE DATABASE IF NOT EXISTS \`${DB}\`;`);
 
 const sequelizeCC = new Sequelize(DB, USER, PASSWORD, {
   host: HOST,
